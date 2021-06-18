7234189758d29a5adfb6d2cdcab339d2b0ac5d36
Desoul2411 <Desoul2411@gmail.com>
2021-06-18 16:19:54 +0300

    replace .test.env to e2e tests folder

--- test/settings/env-setup.ts
+++ test/settings/env-setup.ts
@@ -2,5 +2,5 @@ import * as dotenv from "dotenv";
 import * as path from "path";
 
 console.log(`============ env-setup Loaded ===========`);
-process.env.DEFAULT_DB_DROP_SCHEMA = 'true';
-dotenv.config({ path: path.resolve(process.cwd(), 'tests', 'settings', '.test.env') });
+//process.env.DEFAULT_DB_DROP_SCHEMA = 'true';
+dotenv.config({ path: path.resolve(process.cwd(), 'test', 'settings', '.test.env') });
